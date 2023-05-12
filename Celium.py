f3b4e0952dfaeae5100824003c6c65c5a37bf6d0
Alex <alexandrahickman455@gmail.com>
2023-05-11 20:35:16 -0400

    Setting up main, Game, level class

---
+++ Celium.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 11 20:05:57 2023
+
+@author: alexa
+"""
+import pygame, sys
+from settings import *
+from level import Level
+
+class Game:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
+        pygame.display.set_caption('Celcius')
+        self.clock = pygame.time.Clock()
+        
+        self.level = Level()
+        
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            self.screen.fill('black')
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
---
+++ main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 11 20:33:22 2023
+
+@author: alexa
+"""
+
